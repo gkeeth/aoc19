6e6b3b2cb531151506a1724daafb81817a18b66c
Graham Keeth <graham.keeth@analog.com>
2019-12-02 10:59:15 -0500

    add solution for 12/01

---
+++ 01/01.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+import argparse
+
+def get_module_masses(massfile):
+    """Parse input file to get masses of each module.
+
+    Input file has the mass of each module on its own line.
+
+    Return list of module masses.
+    """
+
+    module_masses = []
+    with open(massfile, "r") as infile:
+        module_masses = [int(line) for line in infile]
+
+    return module_masses
+
+def calculate_module_fuel(module_mass):
+    """calculate fuel required for module, based on module's mass"""
+
+    fuel = (mass // 3) - 2 # // is floor division
+    return fuel
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="input file containing module masses")
+    args = parser.parse_args()
+
+    module_masses = get_module_masses(args.input)
+    module_fuels = [calculate_module_fuel(mass) for mass in module_masses]
+    total_fuel = sum(module_fuels)
+
+    print(total_fuel)
